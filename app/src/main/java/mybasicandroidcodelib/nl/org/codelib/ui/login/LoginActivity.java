9585f59d4bee7ba4c95319975d10e368ada43096
My  Hello World <13469986047@163.com>
2017-11-27 13:43:14 +0800

    修改代码

--- app/src/main/java/mybasicandroidcodelib/nl/org/codelib/ui/login/LoginActivity.java
+++ app/src/main/java/mybasicandroidcodelib/nl/org/codelib/ui/login/LoginActivity.java
@@ -3,21 +3,18 @@ package mybasicandroidcodelib.nl.org.codelib.ui.login;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
-import android.content.pm.PackageManager;
-import android.os.Message;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
 import android.app.LoaderManager.LoaderCallbacks;
-
 import android.content.CursorLoader;
 import android.content.Loader;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
-
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -43,14 +40,10 @@ import org.ksoap2.transport.HttpTransportSE;
 import java.util.ArrayList;
 import java.util.List;
 
-
 import mybasicandroidcodelib.nl.org.codelib.api.webServices.ServiceConfig;
-import mybasicandroidcodelib.nl.org.codelib.bean.CarListBean;
 import mybasicandroidcodelib.nl.org.codelib.bean.LoginBean;
 import mybasicandroidcodelib.nl.org.codelib.config.Config;
-import mybasicandroidcodelib.nl.org.codelib.ui.CarListActivity;
 import mybasicandroidcodelib.nl.org.codelib.ui.CompanyActivity;
-import mybasicandroidcodelib.nl.org.codelib.ui.main.MainActivity;
 import mybasicandroidcodelib.nl.org.mybasicandroidcodelib.R;
 
 import static android.Manifest.permission.READ_CONTACTS;
